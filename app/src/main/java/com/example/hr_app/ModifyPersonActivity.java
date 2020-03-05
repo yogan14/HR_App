4c51da4e486954943fa884fa11da6f459f156614
Leocario <nicolas.constantin@students.hevs.ch>
2020-03-05 16:01:27 +0100

    add some activities

---
+++ app/src/main/java/com/example/hr_app/ModifyPersonActivity.java
@@ -0,0 +1,15 @@
+package com.example.hr_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModifyPersonActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifyperson);
+    }
+}
