1f1963098a96664bb0ec9a061d9d66e177362a35
Leocario <nicolas.constantin@students.hevs.ch>
2020-03-22 12:22:27 +0100

    Avec les viewModel ça marche mieux

---
+++ app/src/main/java/com/example/hr_app/viewmodel/collaborator/CollaboratorViewModel.java
@@ -0,0 +1,88 @@
+package com.example.hr_app.viewmodel.collaborator;
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.example.hr_app.BaseApp;
+import com.example.hr_app.database.entity.Collaborator;
+import com.example.hr_app.database.repository.CollaboratorRepository;
+import com.example.hr_app.util.OnAsyncEventListener;
+
+
+public class CollaboratorViewModel extends AndroidViewModel {
+
+    private CollaboratorRepository repository;
+
+    private Application application;
+
+    // MediatorLiveData can observe other LiveData objects and react on their emissions.
+    private final MediatorLiveData<Collaborator> observableCollaborator;
+
+    public CollaboratorViewModel(@NonNull Application application,
+                                 final int idCollaborator, CollaboratorRepository collaboratorRepository) {
+        super(application);
+
+        this.application = application;
+
+        repository = collaboratorRepository;
+
+        observableCollaborator = new MediatorLiveData<>();
+        // set by default null, until we get data from the database.
+        observableCollaborator.setValue(null);
+
+        LiveData<Collaborator> collaborator = repository.getOneCollaborator(application, idCollaborator);
+
+        // observe the changes of the client entity from the database and forward them
+        observableCollaborator.addSource(collaborator, observableCollaborator::setValue);
+    }
+
+    /**
+     * A creator is used to inject the account id into the ViewModel
+     */
+    public static class Factory extends ViewModelProvider.NewInstanceFactory {
+
+        @NonNull
+        private final Application application;
+
+        private final int idCollaborator;
+
+        private final CollaboratorRepository repository;
+
+        public Factory(@NonNull Application application, int idCollaborator) {
+            this.application = application;
+            this.idCollaborator = idCollaborator;
+            repository = ((BaseApp) application).getCollaboratorRepository();
+        }
+
+        @Override
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            //noinspection unchecked
+            return (T) new CollaboratorViewModel(application, idCollaborator, repository);
+        }
+    }
+
+    /**
+     * Expose the LiveData Collaborator query so the UI can observe it.
+     */
+    public LiveData<Collaborator> getOneCollaborator() {
+        return observableCollaborator;
+    }
+
+    public void insert(Collaborator collaborator, OnAsyncEventListener callback) {
+        repository.insert(collaborator, callback, application);
+    }
+
+    public void update(Collaborator collaborator, OnAsyncEventListener callback) {
+        repository.update(collaborator, callback, application);
+    }
+
+    public void delete(Collaborator collaborator, OnAsyncEventListener callback) {
+        repository.delete(collaborator, callback, application);
+
+    }
+}
